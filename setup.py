39bcd4bb19118175244109014ca6f608a8c1301d
Joseph Rudoler <40604402+jrudoler@users.noreply.github.com>
2021-12-13 18:55:23 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from distutils.core import setup
 setup(name='cmldask',
       version='0.1',
+      author='Joseph Rudoler',
+      author_email=['jrudoler56@gmail.com', 'kahana-sysadmin@gmail.com'],
       py_modules=['cmldask'],
       )
