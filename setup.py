bec2fc23d93538b2b1de6e41ca021397a7e41fc5
Joseph Rudoler <40604402+jrudoler@users.noreply.github.com>
2022-03-31 16:51:15 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
-from distutils.core import setup
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
 setup(name='cmldask',
       version='0.1',
+      packages=find_packages(),
       author='Joseph Rudoler',
       author_email=['jrudoler56@gmail.com', 'kahana-sysadmin@gmail.com'],
       py_modules=['cmldask'],
