b240c7282c5e273bac3774b7bb69336e5fd2a8af
Joseph Rudoler <40604402+jrudoler@users.noreply.github.com>
2022-03-31 17:00:08 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,12 @@ try:
 except ImportError:
     from distutils.core import setup, find_packages
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()    
+
 setup(name='cmldask',
       version='0.1',
+      install_requires=required,
       packages=find_packages(),
       author='Joseph Rudoler',
       author_email=['jrudoler56@gmail.com', 'kahana-sysadmin@gmail.com'],
