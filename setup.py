f5c9135e476a03389809e38d8bb5f997b931c473
Ryan A. Colyer <ryancolyer@yahoo.com>
2023-03-30 10:45:39 -0400

    Bump to v1.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('requirements.txt') as f:
     required = f.read().splitlines()    
 
 setup(name='cmldask',
-      version='0.1',
+      version='1.0',
       install_requires=required,
       packages=find_packages(),
       author='Joseph Rudoler',
